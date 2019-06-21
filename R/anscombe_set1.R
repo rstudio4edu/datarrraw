681132a3178b1e715ecd5ee551be482beafef368
Alison Hill <apreshill@gmail.com>
2019-06-21 13:06:15 -0700

    usethis::use_r("anscombe_set1")

---
+++ R/anscombe_set1.R
@@ -0,0 +1,12 @@
+#' Anscombe's quartet, set 1
+#'
+#' From the datasets package
+#'
+#' @format A data frame with 11 rows and 2 variables:
+#' \describe{
+#'   \item{x1}{the x's}
+#'   \item{y1}{the y's}
+#'   ...
+#' }
+#' @source \url{https://stat.ethz.ch/R-manual/R-devel/RHOME/library/datasets/html/anscombe.html}
+"anscombe_set1"
