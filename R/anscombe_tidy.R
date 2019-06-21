b9ea16591930f7fbcfee57c96417aabc04c13060
Alison Hill <apreshill@gmail.com>
2019-06-21 14:15:26 -0700

    usethis::use_r("anscombe_tidy")

---
+++ R/anscombe_tidy.R
@@ -0,0 +1,14 @@
+#' Anscombe's quartet, tidied
+#'
+#' From the datasets package
+#'
+#' @return a [tibble][tibble::tibble-package]
+#' @format A data frame with 44 rows and 4 variables:
+#' \describe{
+#'   \item{observation}{which point}
+#'   \item{set}{one, two, three, or four as roman numerals}
+#'   \item{x1}{the x's}
+#'   \item{y1}{the y's}
+#' }
+#' @source \url{https://stat.ethz.ch/R-manual/R-devel/RHOME/library/datasets/html/anscombe.html}
+"anscombe_tidy"
